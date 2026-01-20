7e3f92514f059b8c76ea39d45b1f098fd147eee2
yahikii <yasminhinel@gmail.com>
2026-01-20 19:23:34 +0100

    fix: remove buggy escaped password function

--- downloaders/sabnzbd_entrypoint.sh
+++ downloaders/sabnzbd_entrypoint.sh
@@ -5,13 +5,10 @@ export EWEKA_URI=$(cat /run/secrets/eweka_uri)
 export EWEKA_USER=$(cat /run/secrets/eweka_user)
 export EWEKA_PASS=$(cat /run/secrets/eweka_user_password)
 
-# Escape special characters in password 
-ESCAPED_PASS=$(printf '%s\n' "$EWEKA_PASS" | sed -e 's/[\/&]/\\&/g')
-
 # Generate SABnzbd config from template
 sed -e "s|__EWEKA_URI__|$EWEKA_URI|g" \
     -e "s|__EWEKA_USERNAME__|$EWEKA_USER|g" \
-    -e "s|__EWEKA_PASSWORD__|$ESCAPED_PASS|g" \
+    -e "s|__EWEKA_PASSWORD__|$EWEKA_PASS|g" \
     ./templates/sabnzbd.ini > ./config/sabnzbd.ini
 
 # Remove env vars
